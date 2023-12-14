323dc1309fcff7888171ac80ca221c1bf66137e3
Jmz89 <150055331+Jmz89@users.noreply.github.com>
2023-12-14 09:18:36 -0500

    animations
    all axe goblins animations are done

---
+++ My project/Assets/Prefab/enemy/movement and ai/meleattack.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meleattack : MonoBehaviour
+{
+  [SerializeField] private float attackcooldown;
+  [SerializeField] private float damage;
+}
