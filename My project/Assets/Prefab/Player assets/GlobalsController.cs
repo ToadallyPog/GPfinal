a3d6f76874ad024087b800d5c1562d6c24cc5c9d
ToadallyPog <114781827+ToadallyPog@users.noreply.github.com>
2023-12-14 21:02:54 -0500

    backup is correct

---
+++ My project/Assets/Prefab/Player assets/GlobalsController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalsController : MonoBehaviour
+{
+    public static bool defaultComplete = true,
+                        forestOneComplete = false, forestTwoComplete = false, forstCaveComplete = false,
+                        mineOneComplete = false, mineTwoComplete = false, mineCaveComplete = false,
+                        iceOneComplete = false, iceTwoComplete = false, IceCaveComplete = false;
+
+
+}
